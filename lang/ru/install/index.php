884b4aee6ccc4b07c2fe6d644972a587addb8c94
Alexander <at@ranx.ru>
2026-01-18 11:02:52 +0300

    Правки описания

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -4,6 +4,6 @@
  */
 
 $MESS['ONLINELY_PRIMECORP_MODULE_NAME']   = 'Primecorp - универсальный сайт-визитка с каталогом товаров';
-$MESS['ONLINELY_PRIMECORP_MODULE_DESCRIPTION']   = 'Универсальный сайт-визитка. Главная страница с большими баннерами, адаптивный дизайн';
+$MESS['ONLINELY_PRIMECORP_MODULE_DESCRIPTION']   = 'Универсальный сайт-визитка с адаптивным дизайном';
 $MESS['ONLINELY_PRIMECORP_PARTNER_NAME']   = "Интернет агентство «ОНЛАЙНЛИ.РУ»";
 $MESS['ONLINELY_PRIMECORP_PARTNER_URI']   = 'https://onlinely.ru';
