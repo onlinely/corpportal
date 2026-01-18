a4ddb36ddb059b5a78664c274f4a79cf9a18b56f
Alexander <at@ranx.ru>
2026-01-18 10:33:54 +0300

    Правки модуля

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -3,7 +3,7 @@
  * Lang file for install/index.php
  */
 
-$MESS['ONLINELY_PRIMECORP_MODULE_NAME']   = 'primecorp - Универсальный сайт-визитка с каталогом товаров';
+$MESS['ONLINELY_PRIMECORP_MODULE_NAME']   = 'Primecorp - универсальный сайт-визитка с каталогом товаров';
 $MESS['ONLINELY_PRIMECORP_MODULE_DESCRIPTION']   = 'Универсальный сайт-визитка. Главная страница с большими баннерами, адаптивный дизайн';
 $MESS['ONLINELY_PRIMECORP_PARTNER_NAME']   = "Интернет агентство «ОНЛАЙНЛИ.РУ»";
 $MESS['ONLINELY_PRIMECORP_PARTNER_URI']   = 'https://onlinely.ru';
