a11211fd220ca3420eb179c4c1aedbd56275f383
Alexander <at@ranx.ru>
2026-01-18 11:11:11 +0300

    Правки описания

--- install/wizards/onlinely/primecorp/lang/ru/.description.php
+++ install/wizards/onlinely/primecorp/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
-$MESS["PRIMECORP_WIZARD_NAME"] = "Primecorp - универсальный сайт-визитка с каталогом товаров";
+$MESS["PRIMECORP_WIZARD_NAME"] = "PrimeCorp - универсальный сайт-визитка с каталогом товаров";
 $MESS["PRIMECORP_WIZARD_DESC"] = "Мастер создания сайта-визитки";
 ?>
--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -3,7 +3,7 @@
  * Lang file for install/index.php
  */
 
-$MESS['ONLINELY_PRIMECORP_MODULE_NAME']   = 'Primecorp - универсальный сайт-визитка с каталогом товаров';
+$MESS['ONLINELY_PRIMECORP_MODULE_NAME']   = 'PrimeCorp - универсальный сайт-визитка с каталогом товаров';
 $MESS['ONLINELY_PRIMECORP_MODULE_DESCRIPTION']   = 'Универсальный сайт-визитка с адаптивным дизайном';
 $MESS['ONLINELY_PRIMECORP_PARTNER_NAME']   = "Интернет агентство «ОНЛАЙНЛИ.РУ»";
 $MESS['ONLINELY_PRIMECORP_PARTNER_URI']   = 'https://onlinely.ru';
